f6b6ebad48b500aab381ca7edbf9acde5ea905bd
tlelepvriercussol <tlelepvriercussol@enseirb-matmeca.fr>
2015-01-15 18:55:14 +0100

    oublié d'enrengistrer le fichier avant de commit .. #Boulet

--- neural_network_project.py
+++ neural_network_project.py
@@ -3,24 +3,28 @@ import math
 import random
 
 #---------------------- Data Class ----------------------#
-class rodent:
+class mammo:
     
 
     def __init__(self):
-        self.name=""
-        self.size=0.0
-        self.weight=.0
+        self.BI-RADS = 0
+        self.age=0
+        self.shape=0
+        self.margin=0
+        self.density=0
+        self.severity=0
 
-    def set_characteristics(self, name, size, weight):
-        self.name=name
-        self.size=size
-        self.weight=weight
+    def set_characteristics(self, BI-RADS, age, shape, margin, density, severity):
+        self.BI-RADS=BI-RADS
+        self.age=age
+        self.shape=shape
+        self.margin=margin
+        self.density=density
+        self.severity=severity
 
     def get_response(self):
-        if self.name == "A":
-            return 1
-        else:
-            return 0
+        return severity
+
 
 #---------------------- Neuralis Class ----------------------#
 class neuron:
@@ -28,15 +32,14 @@ class neuron:
     learning_rate = 1
     
     def __init__(self):
-        self.entries=[1.0,0.0,0.0]
-        self.weights=[1.0,1.0,1.0]
+        self.entries=[1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.weights=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
         self.threshold=1.0
         self.output=0.0
 
     def init_weights(self):
-        self.weights[0]=random.uniform(-100.0,100.0)
-        self.weights[1]=random.uniform(-100.0,100.0)
-        self.weights[2]=random.uniform(-100.0,100.0)
+        for i in range(len(weights)):
+            self.weights[i]=random.uniform(-10.0,10.0)
         self.threshold=1.0
 
     def calculate(self):
@@ -53,15 +56,17 @@ class neuron:
             #print(self.weights)
         
                   
-    def calculate_output(self, attr1, attr2):
+    def calculate_output(self, attr1, attr2, attr3, attr4):
         self.entries[1]=attr1
         self.entries[2]=attr2
+        self.entries[3]=attr3
+        self.entries[4]=attr4
         
         self.calculate()
 
 #---------------------- Learning Master ----------------------#
 
-def perceptron(listrodents, neuronLambda):
+def perceptron(listmammos, neuronLambda):
     nb_errors=12
     ite=0
     output=0.0
@@ -71,16 +76,16 @@ def perceptron(listrodents, neuronLambda):
         
 #        print neuronLambda.weights
 
-        for rodent in listrodents:
-            neuronLambda.calculate_output(rodent.size, rodent.weight)
+        for mammo in listmammos:
+            neuronLambda.calculate_output(mammo.size, mammo.weight)
             output = neuronLambda.output
-            true_answer = rodent.get_response()
+            true_answer = mammo.get_response()
 
 
             for j in range(len(neuronLambda.weights)):
                 neuronLambda.weights[j] += neuron.learning_rate*(true_answer - output)*neuronLambda.entries[j]
-                """            if true_answer - output != 0:
-                print rodent.size, rodent.weight
+                """if true_answer - output != 0:
+                print mammo.size, mammo.weight
                 print neuron.learning_rate*(true_answer - output)*neuronLambda.entries[j]
                 print neuronLambda.weights"""
             nb_errors += math.fabs(true_answer - output)
@@ -91,25 +96,25 @@ def perceptron(listrodents, neuronLambda):
     print(ite)
 #---------------------- Learning Data ----------------------#
 
-listrodents = [ rodent() for i in range(12)]
+listmammos = [ mammo() for i in range(12)]
 
-#Initalizing listrodents with real data:
+#Initalizing listmammos with real data:
 
-listrodents[0].set_characteristics("A", 36, 17)
-listrodents[1].set_characteristics("A", 41, 16)
-listrodents[2].set_characteristics("A", 48, 18)
-listrodents[3].set_characteristics("A", 47, 18)
-listrodents[4].set_characteristics("A", 50, 22)
-listrodents[5].set_characteristics("A", 48, 21)
-listrodents[6].set_characteristics("B", 48, 12)
-listrodents[7].set_characteristics("B", 56, 16)
-listrodents[8].set_characteristics("B", 62, 13)
-listrodents[9].set_characteristics("B", 60, 24)
-listrodents[10].set_characteristics("B", 65, 18)
-listrodents[11].set_characteristics("B", 66, 21)
+listmammos[0].set_characteristics("A", 36, 17)
+listmammos[1].set_characteristics("A", 41, 16)
+listmammos[2].set_characteristics("A", 48, 18)
+listmammos[3].set_characteristics("A", 47, 18)
+listmammos[4].set_characteristics("A", 50, 22)
+listmammos[5].set_characteristics("A", 48, 21)
+listmammos[6].set_characteristics("B", 48, 12)
+listmammos[7].set_characteristics("B", 56, 16)
+listmammos[8].set_characteristics("B", 62, 13)
+listmammos[9].set_characteristics("B", 60, 24)
+listmammos[10].set_characteristics("B", 65, 18)
+listmammos[11].set_characteristics("B", 66, 21)
 
 
-#print(listrodents[5].weight)
+#print(listmammos[5].weight)
 
 #---------------------- Creating Neurons ----------------------#
 
@@ -120,14 +125,14 @@ neuronLambda.init_weights()
 #---------------------- Testing string comparison ----------------------#
 #print("A"=="B") # false
 #print("A"=="A") # true
-#print("A"==listrodents[0].name) # true
-#print("A"==listrodents[6].name) # false
+#print("A"==listmammos[0].name) # true
+#print("A"==listmammos[6].name) # false
 
 #All working well with ==
 
 #---------------------- Using Neural Network ----------------------#
 
 print(neuronLambda.weights)
-perceptron(listrodents, neuronLambda)
+perceptron(listmammos, neuronLambda)
 print(neuronLambda.weights)
 
